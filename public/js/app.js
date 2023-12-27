323872bb73a804bc9eb67396d132a667016c037b
Hlxrd1~ <hamzayasri359@gmail.com>
2023-12-27 02:05:40 +0100

    password confirmation

--- public/js/app.js
+++ public/js/app.js
@@ -8,53 +8,60 @@ let bankDataBase = [];
 
 const userSignUp =()=> {
     // //* fullname check
-    //     let nom = prompt(`Your full name`);
-    //     while(nom.split(" ").join("").length < 5 ) {
+        let nom = prompt(`Your full name`);
+        while(nom.split(" ").join("").length < 5 ) {
             
-    //         nom = prompt(`your name is not valide / should containe more than 5 characters `)
-    //     };
+            nom = prompt(`your name is not valide / should containe more than 5 characters `)
+        };
 
-    //     while(nom.includes("*"||"/"||`!` ||"@"||"#"||"$"||"%"||"^"||"&"||"*"||"()"||"_")) {
-    //         nom = prompt(`your name is not valide / should not containe numbers or special characters such as : @ / * ,`)
-    //     };
+        while(nom.includes("*"||"/"||`!` ||"@"||"#"||"$"||"%"||"^"||"&"||"*"||"()"||"_")) {
+            nom = prompt(`your name is not valide / should not containe numbers or special characters such as : @ / * ,`)
+        };
 
-    //     let tableau = nom.split(" ");
-    //     for (let i = 0; i < tableau.length; i++) {
-    //         tableau[i] = tableau[i].charAt(0).toUpperCase() + tableau[i].slice(1).toLocaleLowerCase();
-    //     }
-    //     nom = tableau.join(" ");
+        let tableau = nom.split(" ");
+        for (let i = 0; i < tableau.length; i++) {
+            tableau[i] = tableau[i].charAt(0).toUpperCase() + tableau[i].slice(1).toLocaleLowerCase();
+        }
+        nom = tableau.join(" ");
         
 
     // //* email check
 
-    // let email = prompt(`your unique Email`);
-    // email = email.trim().toLocaleLowerCase();
-    // if (email === "") {
-    //     email = prompt(`make sure you entered your email`);
-    // }else if (email.includes(" ")) {
-    //     email = prompt(`avoide using spaces inside email adress`);
-    // }else if (email.indexOf("@") === -1) {
-    //     email = prompt(`use the character "@" to complete you email`);
-    // }
-    // if (bankDataBase.some(user => user.email === email)) {
-    //     email = (`email already exists please enter a valide email`);
-    // }
+    let email = prompt(`your unique Email`);
+    email = email.trim().toLocaleLowerCase();
+    if (email === "") {
+        email = prompt(`make sure you entered your email`);
+    }else if (email.includes(" ")) {
+        email = prompt(`avoide using spaces inside email adress`);
+    }else if (email.indexOf("@") === -1) {
+        email = prompt(`use the character "@" to complete you email`);
+    }
+    if (bankDataBase.some(user => user.email === email)) {
+        email = (`email already exists please enter a valide email`);
+    }
 
     // //* age check
 
-    // let age = parseInt(prompt(`Enter your age please`).trim());
-    // if (age.length === 0 || age.length >= 3) {
-    //     email = prompt(`please enter a valide age`);
-    // }
+    let age = parseInt(prompt(`Enter your age please`).trim());
+    if (age.length === 0 || age.length >= 3) {
+        email = prompt(`please enter a valide age`);
+    }
     
     //* password check
     
     let password = prompt(`Choose a password`);
     password = password.trim();
     if (password.includes(` `) || password.length < 7 || password.includes("*"||"/"||`!` ||"@"||"#"||"$"||"%"||"^"||"&"||"*"||"()"||"_")) {
-        password = prompt(`unvalide password . please avoide using spaces , using specific characters or entering less than 7 characters`)
-    }
-    let newUser = {nom , email , age};
+        password = prompt(`unvalide password . please avoide using spaces , using specific characters or entering less than 7 characters`);
+    };
+
+    //* password confirmation
+    let confirmPassword = prompt(`please confirme your password`);
+if (confirmPassword != password) {
+    alert(`You have been blocked`);
+    userSignUp();
+}
+    let newUser = {nom , email , age , password};
     newUser.money = 2000;
     bankDataBase.push(newUser)
 }
