4b4ca5514c0f062e58a90af01dd9bd9f143103ae
Hlxrd1~ <hamzayasri359@gmail.com>
2023-12-27 01:28:49 +0100

    finishing up email check

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,9 @@ let bankDataBase = [];
 
 
 
-let userChoice = prompt(`type "login" to enter to your account || "signup" to creat your account || "change password" to set a new password`);
+// let userChoice = prompt(`type "login" to enter to your account || "signup" to creat your account || "change password" to set a new password`);
 
-if (userChoice == `signup`) {
+const userSignUp =()=> {
     // //* fullname check
     // let nom = prompt(`Your full name`);
     // while(nom.split(" ").join("").length < 5 ) {
@@ -27,17 +27,20 @@ if (userChoice == `signup`) {
 
     //* email check
 
-    let email = prompt(`your unique Email`)
-    let emailcopy = email.trim().toLocaleLowerCase();
-    while(emailcopy.split("") === email.includes(" ")) {
-        email = prompt(`enter your email without using spaces in the middle`);
-        
-    };
-    
-    let emailcheck = email.split("")
-    while(emailcheck != email.includes("@")) {
-            email = prompt(`enter your email  using only one "@"`)
-            
+    let email = prompt(`your unique Email`);
+    email = email.trim().toLocaleLowerCase();
+    if (email === "") {
+        email = prompt(`make sure you entered your email`);
+    }else if (email.includes(" ")) {
+        email = prompt(`avoide using spaces inside email adress`);
+    }else if (email.indexOf("@") === -1) {
+        email = prompt(`use the character "@" to complete you email`)
     }
+
     console.log(email);
+
+    // let age = prompt(`Enter your age please`).trim();
+    // age = 
+    // console.log(age);
 }
+userSignUp()
