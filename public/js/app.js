d009909924e19d010fd4b54bff5db0500f52031a
Hlxrd1~ <hamzayasri359@gmail.com>
2023-12-27 01:46:33 +0100

    age check and account creation

--- public/js/app.js
+++ public/js/app.js
@@ -8,22 +8,22 @@ let bankDataBase = [];
 
 const userSignUp =()=> {
     // //* fullname check
-    // let nom = prompt(`Your full name`);
-    // while(nom.split(" ").join("").length < 5 ) {
+        let nom = prompt(`Your full name`);
+        while(nom.split(" ").join("").length < 5 ) {
+            
+            nom = prompt(`your name is not valide / should containe more than 5 characters `)
+        };
+
+        while(nom.includes("*"||"/"||`!` ||"@"||"#"||"$"||"%"||"^"||"&"||"*"||"()"||"_")) {
+            nom = prompt(`your name is not valide / should not containe numbers or special characters such as : @ / * ,`)
+        };
+
+        let tableau = nom.split(" ");
+        for (let i = 0; i < tableau.length; i++) {
+            tableau[i] = tableau[i].charAt(0).toUpperCase() + tableau[i].slice(1).toLocaleLowerCase();
+        }
+        nom = tableau.join(" ");
         
-    //     nom = prompt(`your name is not valide / should containe more than 5 characters `)
-    // };
-
-    // while(nom.includes("*"||"/"||`!` ||"@"||"#"||"$"||"%"||"^"||"&"||"*"||"()"||"_")) {
-    //     nom = prompt(`your name is not valide / should not containe numbers or special characters such as : @ / * ,`)
-    // };
-
-    // let tableau = nom.split(" ");
-    // for (let i = 0; i < tableau.length; i++) {
-    //     tableau[i] = tableau[i].charAt(0).toUpperCase() + tableau[i].slice(1).toLocaleLowerCase();
-    // }
-    // let nom2 = tableau.join(" ");
-    // console.log(nom2);
 
     //* email check
 
@@ -34,13 +34,21 @@ const userSignUp =()=> {
     }else if (email.includes(" ")) {
         email = prompt(`avoide using spaces inside email adress`);
     }else if (email.indexOf("@") === -1) {
-        email = prompt(`use the character "@" to complete you email`)
+        email = prompt(`use the character "@" to complete you email`);
+    }
+    if (bankDataBase.some(user => user.email === email)) {
+        email = (`email already exists please enter a valide email`);
     }
 
-    console.log(email);
+    //* age check
 
-    // let age = prompt(`Enter your age please`).trim();
-    // age = 
-    // console.log(age);
+    let age = parseInt(prompt(`Enter your age please`).trim());
+    if (age.length === 0 || age.length >= 3) {
+        email = prompt(`please enter a valide age`);
+    }
+    let newUser = {nom , email , age};
+    newUser.money = 2000;
+    bankDataBase.push(newUser)
 }
 userSignUp()
+console.log(bankDataBase);
